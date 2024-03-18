9b963bb1f403815e0b0484e0520213262d72cd25
Maria Isabel <129692537+mariaisabelcoms@users.noreply.github.com>
2024-03-18 13:44:20 +0000

    novo commit

--- import.py
+++ import.py
@@ -1,10 +1,50 @@
 import pandas as pd
 
-df = pd.read_csv('./top10s.csv')
+df = pd.read_csv("/workspaces/data-science/top10s.csv", encoding='ISO-8859-1')
 
-dfs = pd.read_csv('/workspaces/data-science/SpotifyTopSongsByCountry - May 2020.csv')
+dfs = pd.read_csv('/workspaces/data-science/SpotifyTopSongsByCountry - May 2020.csv', encoding='ISO-8859-1')
 
-df.head()
+# cabeca1 = df.head()
+
+# print(cabeca1)
+
+# cabeca2 = dfs.head()
+
+# print(cabeca2)
+
+# rabo = df.tail(10)
+
+# print(rabo)
+
+# print(df.info)
+
+# print(df.dtypes)
+
+# print(df.select_dtypes(include='object'))
+
+# print(df.describe)
+
+# print(df.isna().sum())
+
+# print(df['artist'].unique())
+
+# print(df['artist'].nunique())
+
+# print(df.shape)
+
+# print(df.size)
+
+# print(df.columns)
+
+# print(df.nsmallest(3, 'bpm'))
+
+# print(df.nlargest(3, 'bpm'))
+
+df_ops=df.drop_duplicates(subset=['title'], keep='last')
+
+print(df_ops)
+
+
 
 
 
