dc1d4e4a30c919be35dc8a4367652f2f44ec2f24
Maria Isabel <129692537+mariaisabelcoms@users.noreply.github.com>
2024-03-19 15:38:08 +0000

    commit 1903

--- import.py
+++ import.py
@@ -42,7 +42,17 @@ dfs = pd.read_csv('/workspaces/data-science/SpotifyTopSongsByCountry - May 2020.
 
 df_ops=df.drop_duplicates(subset=['title'], keep='last')
 
-print(df_ops)
+# print(df_ops)
+
+# print(df.query("artist == 'Ariana Grande'"))
+
+# print(df.query('dur > 200'))
+
+df_ops.drop(columns=['spch'], inplace=True)
+
+print(df.groupby('artist').count())
+
+
 
 
 
